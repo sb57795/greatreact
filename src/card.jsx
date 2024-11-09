76c70d904d13a9800cde37ec76159f0386d81de0
sb57795 <134415146+sb57795@users.noreply.github.com>
2024-11-10 02:05:31 +0300

    react

---
+++ src/card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+function Card(props){
+    return (
+        <div className='form'>
+          <h1 className='form-title'>Stork</h1>
+          <h2 className='form-text'>[stok]</h2>
+          <button className='form-buttt'>Проверить</button>
+        </div>
+          );
+}
+export default Card
